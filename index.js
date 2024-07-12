62bbb0fa9bad68569e520b2256aa8981ac2536a2
arishgautam <arishgautam686@gmail.com>
2024-07-12 19:40:08 +0545

    added log

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-import 'dotenv/config';
+import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import { customLogger } from "./middleware/morganLogger.js";
 // app config
 const app = express();
-const port = process.env.PORT||4000;
-
-// middleware
-app.use(express.json());
-app.use(cors());
+const port = process.env.PORT || 4000;
 
 // db connection
 connectDB();
 
+// middleware
+app.use(customLogger);
+app.use(cors());
+app.use(express.json());
+
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
-app.use("/api/user", userRouter); 
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API working");
+  res.send("API working");
 });
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
