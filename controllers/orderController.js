a8636c76a4060fb824e1de08f0aa7ae8db296e19
arishgautam <arishgautam686@gmail.com>
2024-07-26 12:06:49 +0545

    error solving

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ import orderModel from "../models/ordermodel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
+//stripe payment method
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
