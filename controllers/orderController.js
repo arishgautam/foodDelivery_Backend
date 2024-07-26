9505ebe539b440805758b6c97291e9e7d501d5bd
arishgautam <arishgautam686@gmail.com>
2024-07-26 11:16:17 +0545

    added stripr payment method

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,116 +1,88 @@
 import orderModel from "../models/ordermodel.js";
-
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 
 // Placing user order from frontend
-const placeOrder = async (req, res) => {
+const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174"
+const frontend_url = "http://localhost:5174"
 
-    //! Stripe Payment
-    // try {
-    //     const newOrder = new orderModel({
-    //         userId: req.body.userId,
-    //         items: req.body.items,
-    //         amount: req.body.amount,
-    //         address: req.body.address
-    //     })
-    //     await newOrder.save();
-    //     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+try {
+    const newOrder = new orderModel({
+        userId:req.body.userId,
+        items:req.body.items,
+        amount:req.body.amount,
+        address:req.body.address
+    })
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
 
-    //     const line_items = req.body.items.map((item) => ({
-    //         price_data: {
-    //             currency: "Npr",
-    //             product_data: {
-    //                 name: item.name
-    //             },
-    //             unit_amount: item.price * 200
-    //         },
-    //         quantity: item.quantity
-    //     }))
-    //     line_items.push({
-    //         price_data: {
-    //             currency: "Npr",
-    //             product_data: {
-    //                 name: "Delivery Charges"
-    //             },
-    //             unit_amount: 2 * 200
-    //         },
-    //         quantity: 1
-    //     })
+    const line_items = req.body.items.map((item)=>({
+price_data:{
+    currency:"Npr",
+    product_data:{
+        name:item.name
+    },
+    unit_amount:item.price*100*133
+},
+quantity:item.quantity
+    }))
+    line_items.push({
+        price_data:{
+            currency:"Npr",
+            product_data:{
+                name:"Delivery Charges"
+            },
+            unit_amount:2*100*133
+        },
+        quantity:1
+    })
 
-    //     const session = await khalti.checkout.session.create({
-    //         line_items: line_items,
-    //         mode: 'payment',
-    //         success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-    //         cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-    //     })
+const session = await stripe.checkout.session.create({
+    line_items:line_items,
+    mode:'payment',
+    success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+    cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+})
 
-    //     res.json({ success: true, session_url: session_url })
+res.json({success:true,session_url:session_url})
 
-    // } catch (error) {
-    //     console.log(error);
-    //     res.json({ success: false, message: "Error" })
-    // }
-
-    //! Esewa Integration
-    try {
-        console.log(req.body);
-        const order = await orderService.save(req.body);
-        const signature = this.createSignature(
-            `total_amount=${order.amount},transaction_uuid=${order_.id},product_code=EPAYTEST`
-        );
-
-        const formtData = {
-            amount: order.amount,
-            failure_url: 'http://localhost:5173',
-            product_delivery_charge: "0",
-            product_service_charge: "0",
-            product_code: "PAYTEST",
-            signature: signature,
-            signed_field_names: "total_amount,transaction_uuid,product_code",
-            successurl: "https://localhost:5005/api/esewa/success",
-            tax_amount: "0",
-            total_amount: order.amount,
-            transaction_uuid: order_.id,
-        };
-        res.json({ message: "Order Created Successfully", order, formData })
-    }
-    catch (err) {
-        return res.status(400).json({ error: err?.message || "No Orders Found" });
-    }
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
 }
 
-
-const verifyOrder = async (req, res) => {
-    const { orderId, success } = req.body;
-    try {
-        if (success == "true") {
-            await orderModel.findByIdAndUpdate(orderId, { payment: true });
-            res.json({ success: true, message: "Paid" })
-        }
-        else {
-            await orderModel.findByIdAndDelete(orderId);
-            res.json({ success: false, message: "Not Paid" })
-        }
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
+const verifyOrder = async (req,res) => {
+const {orderId,success} = req.body;
+try {
+    if (success=="true") {
+        await orderModel.findByIdAndUpdate(orderId,{payment:true});
+        res.json({success:true,message:"Paid"})
     }
+    else{
+        await orderModel.findByIdAndDelete(orderId);
+        res.json({success:false,message:"Not Paid"})
+    }
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
 }
 
 // user order for frontend
-const userOrders = async (req, res) => {
-    try {
-        const orders = await orderModel.find({ userId: req.body.userId })
-        res.json({ success: true, data: orders })
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
+const userOrders = async (req,res) => {
+try {
+    const orders = await orderModel.find({userId:req.body.userId})
+    res.json({success:true,data:orders})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
 }
 
 // listing orders for admin pannel
