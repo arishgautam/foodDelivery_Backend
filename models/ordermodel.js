f1ca2431c420e6995d28b93c08fd06e80e430bd1
arishgautam <arishgautam686@gmail.com>
2024-07-27 08:33:09 +0545

    stripe success

--- models/ordermodel.js
+++ models/ordermodel.js
@@ -1,44 +1,14 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
-    {
-        payment_method: {
-            type: String,
-            required: true,
-            default: "esewa",
-        },
-        transaction_code: String,
-        amount: {
-            type: Number,
-            required: true,
-        },
-        products: [
-            {
-                product: {
-                    type: String,
-                    required: true,
-                    default: "Test",
-                },
-                quantity: {
-                    type: Number,
-                    required: true,
-                    default: 1,
-                },
-            },
-        ],
-        status: {
-            type: String,
-            required: true,
-            enum: ["created", "paid", "shipping", "delivered"],
-            default: "created",
-        },
-        address: String,
-    },
-    {
-        timestamps: true,
-    }
-);
+const orderSchema = new mongoose.Schema({
+    userId:{type:String,requried:true},
+    items:{type:Array,requried:true},
+    amount:{type:Number,requried:true},
+    address:{type:Object,requried:true},
+    status:{type:String,default:"Food Processing"},
+    date:{type:Date,default:Date.now()},
+    payment:{type:Boolean,default:false}
+})
 
-const Order = mongoose.model("Order", orderSchema);
-
-export default Order;
+const orderModel =  mongoose.models.order || mongoose.model("order",orderSchema )
+export default orderModel;
